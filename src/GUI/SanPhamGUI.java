5926f0d756cd5bcaa068ab9a02beff2df19deceb
HeoBayMau <HeoBayMau@DESKTOP-SOGS541>
2017-04-27 19:36:59 +0700

    test edit

--- src/GUI/SanPhamGUI.java
+++ src/GUI/SanPhamGUI.java
@@ -96,7 +96,7 @@ public class SanPhamGUI extends javax.swing.JFrame {
 
         jLabel6.setText("Số Lượng Có");
 
-        tfSoLuongCo.setText("1");
+        tfSoLuongCo.setText("10");
 
         jLabel7.setText("Thông tin hàng hóa");
 
